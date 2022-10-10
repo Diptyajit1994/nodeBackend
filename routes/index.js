cc1532b271ac508426ad0cd4cec5140491c90c2e
Diptyajit Mitra <diptyajitmitra9@gmail.com>
2022-10-10 17:04:39 +0530

    merge check

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const logger = require('../utils/logger').logger;
+const houseInfo = require('../inputFile/houses.json');
 
 router.get('/', async (req, res, next) => {
   try {
@@ -20,5 +21,24 @@ router.get('/', async (req, res, next) => {
     }
   }
 });
+
+router.get('/house-info', (req,res,next)=>{
+  try {
+    logger.info('Get houseinfo fetch is running');
+    const houseData= houseInfo;
+    res.status(200).send({
+      success: true,
+      data: houseData
+    })
+
+  } catch (error) {
+    logger.error(err);
+    res.status(400);
+    res.send({
+      success: false,
+      msg: 'Something went wrong'
+    });
+  }
+})
 
 module.exports = router;
